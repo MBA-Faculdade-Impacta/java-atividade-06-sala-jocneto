d886a51d5e31dac0852a330db8f36d90e4e75e52
jocneto <jocneto20@yahoo.com.br>
2021-03-04 22:36:21 +0000

    arrumar algumas configuraçoes

--- src/main/java/br/com/impacta/lab/models/Produto.java
+++ src/main/java/br/com/impacta/lab/models/Produto.java
@@ -1,15 +1,16 @@
 package br.com.impacta.models
 
 public class produto {
-  public int codigo;
+  private int codigo;
   public string descricao;
   public double valor;
 
   public static Produto buscarProdutoPeloId(ArrayList<Produto> listaDeProdutos, int codigoProduto) {
 		 for (int contador = 0; contador < listadeprodutos.size(); contador++){
        Produto produto = listaDeProdutos.get(contador);
-       if (codigoProduto == 2 produto.codigo)
+       if (codigoProduto == produto.codigo)
 return produto     }
-return null 
+
+{return null} 
 }
 }
